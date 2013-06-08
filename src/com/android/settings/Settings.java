747ebedb666b09cf97bf46eef7760d640a844315
Rakesh Pallerla <rakesh@codeaurora.org>
2013-06-08 16:49:47 +0530

    Multisim: Add support for MSIM
    Add Multi SIM Settings screen in UI which is used to select
    Voice, SMS, Data preferences.
    Extend APN Settings, Security Settings, Wireless Settings
    for MSIM.
    
    Subscription specific for Mobile data, roaming and usage
     - Provide per subscription based support for both Mobile Data
       and Data roaming options. UI and functionality changes in:
       a. Settings -> Data Usage application
       b. Settings -> Mobile Networks menu
     - Provide per subscription based data usage information. UI and
       functionality changes in Settings -> Data Usage application.
    
    Add prompt support for SMS
     - Add prompt option in SMS multi sim settings.
     - New iterface API to get SMS prompt is enabled or not.
     - Disply prompt while sending SMS, if user selects
       prompt mode in multi sim settings
    
    Extend CB "Operator info" feature to MSIM.
     - Add SUB information while broadcasting CB intent for MSIM.
     - Remove country specific checks for cellbroadcast Operator
       info for MSIM.
     - Move Operator info to About phone->Status->SelectSubscription
       ->SUB1/SUB2->Operator info in MSIM
    
    Change-Id: I84abe80f20de2415ff819c7250ef0a0799662b52
    (cherry picked from commit ccfd14b8a86c78c6a179a70be1357131b80d4823)
    (cherry picked from commit 794b5787304bed493bbde3e9c9010a4b82824641)

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -50,6 +50,7 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.text.TextUtils;
+import android.telephony.MSimTelephonyManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -698,6 +699,9 @@ public class Settings extends PreferenceActivity
                 if (!supported) {
                     target.remove(i);
                 }
+            } else if (id == R.id.multi_sim_settings) {
+                if (!MSimTelephonyManager.getDefault().isMultiSimEnabled())
+                    target.remove(header);
             }
 
             if (i < target.size() && target.get(i) == header
