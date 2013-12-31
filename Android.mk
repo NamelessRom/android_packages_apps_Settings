5f478058bd5223b66516597299e5f52bd11854a7
Alexander Martinz <eviscerationls@gmail.com>
2013-12-31 03:54:15 +0100

    include cyanogenmod's hardware library
    Change-Id: I32fad54912ade2d5925a677cc63763e4ad6227df

--- Android.mk
+++ Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_JAVA_LIBRARIES := bouncycastle conscrypt telephony-common
+LOCAL_JAVA_LIBRARIES += org.cyanogenmod.hardware
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13 jsr305
 
 LOCAL_MODULE_TAGS := optional
