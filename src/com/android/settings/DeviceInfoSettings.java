08e525daa8b8dbc42779af6e5e01c7b91fccb9dc
Evisceration <eviscerationls@gmail.com>
2014-02-12 16:19:16 +0100

    Settings: do not show cm easter egg
    Change-Id: Ic5592b146459781749230348028269a9baf8883d

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -204,7 +204,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
                     mDevHitToast.show();
                 }
             }
-        } else if (preference.getKey().equals(KEY_MOD_VERSION)) {
+        } else if (preference.getKey().equals(KEY_MOD_VERSION) && false) {
             System.arraycopy(mHits, 1, mHits, 0, mHits.length-1);
             mHits[mHits.length-1] = SystemClock.uptimeMillis();
             if (mHits[0] >= (SystemClock.uptimeMillis()-500)) {
