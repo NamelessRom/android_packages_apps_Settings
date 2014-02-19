90f2e01a23976f5034730d23259236a08d7b013a
Lars Greiss <kufikugel@googlemail.com>
2014-02-19 10:42:24 +0100

    QS: add theme switcher tile (TRDS) 2/2
    this adds a theme switcher tile
    
    normal click: turn on/off dark/light theme
    longpress: switch between the auto modes (manual/light sensor/twilight)
    
    Change-Id: I5f4bb146e240bf31b6486aeff85b0828ee6fbb09

--- src/com/android/settings/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/quicksettings/QuickSettingsUtil.java
@@ -40,6 +40,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_SCREENTIMEOUT;
 import static com.android.internal.util.cm.QSConstants.TILE_SETTINGS;
 import static com.android.internal.util.cm.QSConstants.TILE_SLEEP;
 import static com.android.internal.util.cm.QSConstants.TILE_SYNC;
+import static com.android.internal.util.cm.QSConstants.TILE_THEME;
 import static com.android.internal.util.cm.QSConstants.TILE_TORCH;
 import static com.android.internal.util.cm.QSConstants.TILE_USER;
 import static com.android.internal.util.cm.QSConstants.TILE_VOLUME;
@@ -153,6 +154,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_NETWORKADB, R.string.title_tile_network_adb,
                 "com.android.systemui:drawable/ic_qs_network_adb_off"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_THEME, R.string.title_tile_theme,
+                "com.android.systemui:drawable/ic_qs_theme_manual"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
