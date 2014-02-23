b2c537121227066c53578ec42e8d2ac5e9cb10d1
Roman Birg <romanbirg@gmail.com>
2014-02-24 00:32:41 +0100

    Settings: randomize "The Crew" list
    Change-Id: I6f191bda2dcd9c739607880dca32854307927097
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- src/com/android/settings/nameless/about/AboutCrewFragment.java
+++ src/com/android/settings/nameless/about/AboutCrewFragment.java
@@ -11,6 +11,8 @@ import android.view.ViewGroup;
 
 import com.android.settings.R;
 
+import java.util.Random;
+
 
 public class AboutCrewFragment extends Fragment {
 
@@ -23,13 +25,20 @@ public class AboutCrewFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_about_nameless_crew, container, false);
 
-        return root;
-    }
+        final Random rng = new Random();
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        // TODO scramble dev list
+        ViewGroup crewGroup = (ViewGroup) root.findViewById(R.id.crew);
+
+        // remove all developers from the view randomize them, add em back
+        int N = crewGroup.getChildCount();
+        while (N > 0) {
+            final View removed = crewGroup.getChildAt(rng.nextInt(N));
+            crewGroup.removeView(removed);
+            crewGroup.addView(removed);
+            N -= 1;
+        }
+
+        return root;
     }
 
     private void launchActivity(String packageName, String activity)
