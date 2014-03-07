14745784c73d0570f6490af45d255a6c74f3245d
Evisceration <eviscerationls@gmail.com>
2014-03-07 10:43:46 +0100

    [3/3] Allow systemwide crash detection using ACRA
    Change-Id: If23f698ad569bf89f47a6ac3a968fcc40960a707

--- src/com/android/settings/nameless/preferences/SystemSettingCheckBoxPreference.java
+++ src/com/android/settings/nameless/preferences/SystemSettingCheckBoxPreference.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.nameless;
+package com.android.settings.nameless.preferences;
 
 import android.content.Context;
 import android.preference.CheckBoxPreference;
