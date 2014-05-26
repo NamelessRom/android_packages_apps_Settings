28a56e956858ac62a8dc6e50972f03c22cd3d86b
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2014-05-26 17:26:40 +0530

    [2/2] Buttons: Switch to last app
    Change-Id: Ie2702104e87fc5f4e9b81856954ab8f40bcb85d1

--- src/com/android/settings/cyanogenmod/ButtonSettings.java
+++ src/com/android/settings/cyanogenmod/ButtonSettings.java
@@ -70,6 +70,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final int ACTION_VOICE_SEARCH = 4;
     private static final int ACTION_IN_APP_SEARCH = 5;
     private static final int ACTION_LAUNCH_CAMERA = 6;
+    private static final int ACTION_LAST_APP = 7;
 
     // Masks for checking presence of hardware keys.
     // Must match values in frameworks/base/core/res/res/values/config.xml
@@ -160,14 +161,14 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             int defaultLongPressAction = res.getInteger(
                     com.android.internal.R.integer.config_longPressOnHomeBehavior);
             if (defaultLongPressAction < ACTION_NOTHING ||
-                    defaultLongPressAction > ACTION_IN_APP_SEARCH) {
+                    defaultLongPressAction > ACTION_LAST_APP) {
                 defaultLongPressAction = ACTION_NOTHING;
             }
 
             int defaultDoubleTapAction = res.getInteger(
                     com.android.internal.R.integer.config_doubleTapOnHomeBehavior);
             if (defaultDoubleTapAction < ACTION_NOTHING ||
-                    defaultDoubleTapAction > ACTION_IN_APP_SEARCH) {
+                    defaultDoubleTapAction > ACTION_LAST_APP) {
                 defaultDoubleTapAction = ACTION_NOTHING;
             }
 
