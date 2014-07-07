32e2eb0bab74f6136942ffa884eb2596cde28bd4
Evisceration <eviscerationls@gmail.com>
2014-07-07 18:39:41 +0200

    add our own BootReceiver, strip it down
    Change-Id: I2476926de2d5dfe5fcd7d0d1eef0d281fb69ee7b

---
+++ src/com/android/settings/nameless/BootReceiver.java
@@ -0,0 +1,51 @@
+/*
+ * <!--
+ *    Copyright (C) 2014 The NamelessRom Project
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * -->
+ */
+
+package com.android.settings.nameless;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.android.settings.DisplaySettings;
+import com.android.settings.hardware.DisplayColor;
+import com.android.settings.hardware.DisplayGamma;
+import com.android.settings.hardware.VibratorIntensity;
+import com.android.settings.location.LocationSettings;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    private static final boolean DEBUG = false;
+
+    private static final String TAG = "BootReceiver";
+
+    @Override
+    public void onReceive(final Context ctx, final Intent intent) {
+        if (DEBUG) Log.d(TAG, "BootReceiver is running");
+
+        /* Restore the hardware tunable values */
+        DisplayColor.restore(ctx);
+        DisplayGamma.restore(ctx);
+        VibratorIntensity.restore(ctx);
+        DisplaySettings.restore(ctx);
+        LocationSettings.restore(ctx);
+    }
+
+}
