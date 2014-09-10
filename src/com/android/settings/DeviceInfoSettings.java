0958ed63a1f58ffb5aa99513b4aaa5b8b277fa00
Shuhrat Dehkanov <k@efir.uz>
2014-09-11 08:21:20 +0900

    Re-enable "(ENGINEERING)" suffix
    "(ENGINEERING)" suffix was appended to Model number in
    Settings > About screen in CL 2397a7391e2ba9434ee89345c1d3a47948d8f226
    (Show in Settings->About if the device is an engineering sample).
    However, it was made absolute (probably, by mistake) in CL
    b0b45c02ec73c87f78a8358a7e98747bd96ec103 (Display FCC id for LTE in Settings).
    This removes the redundant line added in latter CL.
    
    Change-Id: I16cb6a72861c2a220e2e032d068259f98117788b
    Signed-off-by: Shuhrat Dehkanov <k@efir.uz>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -96,7 +96,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setValueSummary(KEY_BASEBAND_VERSION, "gsm.version.baseband");
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL + getMsvSuffix());
         setValueSummary(KEY_EQUIPMENT_ID, PROPERTY_EQUIPMENT_ID);
-        setStringSummary(KEY_DEVICE_MODEL, Build.MODEL);
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         setValueSummary(KEY_MOD_VERSION, "ro.modversion");
