e89a2c6a091968deb8ce04e111a71c0537791b62
jumoog <kilianvonpflugk@gmail.com>
2014-11-24 19:10:13 +0100

    port: Settings: disable camera shutter sound
    Change-Id: I7a0aa41904055d5168dc9171f8e3514e6e17e6a5

--- src/com/android/settings/notification/OtherSoundSettings.java
+++ src/com/android/settings/notification/OtherSoundSettings.java
@@ -151,6 +151,19 @@ public class OtherSoundSettings extends SettingsPreferenceFragment implements In
         }
     };
 
+    private static final SettingPref PREF_CAMERA_SOUNDS = new SettingPref(
+            TYPE_SYSTEM, KEY_CAMERA_SOUNDS, System.CAMERA_SOUND_ENABLED, DEFAULT_ON);
+        @Override
+        protected boolean setSetting(Context context, int value) {
+            if (SystemProperties.set(PROP_CAMERA_SOUND, String.valueOf(value));) {
+                SystemProperties.set(PROP_CAMERA_SOUND, mCameraSounds.isChecked() ? "1" : "0");
+            } else {
+                SystemProperties.set(PROP_CAMERA_SOUND, mCameraSounds.isChecked() ? "0" : "1");
+            }
+            return super.setSetting(context, value);
+        }
+    };
+
     private static final SettingPref[] PREFS = {
         PREF_DIAL_PAD_TONES,
         PREF_SCREEN_LOCKING_SOUNDS,
@@ -159,6 +172,7 @@ public class OtherSoundSettings extends SettingsPreferenceFragment implements In
         PREF_VIBRATE_ON_TOUCH,
         PREF_DOCK_AUDIO_MEDIA,
         PREF_EMERGENCY_TONE,
+        PREF_CAMERA_SOUNDS,
     };
 
     private final SettingsObserver mSettingsObserver = new SettingsObserver();
