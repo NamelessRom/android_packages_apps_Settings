27163a841bb168c9c70ba90d6cbd2808f7bc22b0
Alexander Martinz <eviscerationls@gmail.com>
2015-04-06 18:25:45 +0200

    Update modversion to nameless
    Change-Id: Ic10e838e235e8dfa534ce20ce7261b86bd3cfc28
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -134,8 +134,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
-        findPreference(KEY_MOD_VERSION).setSummary(
-                cyanogenmod.os.Build.CYANOGENMOD_DISPLAY_VERSION);
+        setValueSummary(KEY_MOD_VERSION, "ro.modversion");
         findPreference(KEY_MOD_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
         setExplicitValueSummary(KEY_MOD_API_LEVEL, constructApiLevelString());
