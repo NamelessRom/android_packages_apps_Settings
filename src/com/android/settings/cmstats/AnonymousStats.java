4f9e3e97c10b3aaa1b76876c8a6add05b8b02081
Alexander Martinz <eviscerationls@gmail.com>
2015-11-25 17:48:55 +0100

    (Squashed) revert cm stats to old google analytics implementation
    -----
    
    Revert "Settings: utilize STATS_COLLECTION from CMSettings."
    
    This reverts commit d51702e9cf20be5a0ce6453b43c4e20dad95d97f.
    
    Revert "Settings: add opt out stats event"
    
    This reverts commit 84c44757ac58d285aaaaf019c13c3afe9f664e80.
    
    Change-Id: Ib3c8075ecec6053a7e888b0bc55cb3b2cb0aa9f8
    
    Revert "Settings: report stats upload immediately"
    
    This reverts commit 53fefc5a6515d3903b7ffdfa4f013253ce68009b.
    
    Revert "Settings: fix reversed name/id fields in metrics"
    
    This reverts commit c7a23cb10cc773f21a521dcb62863ee60b1b3e52.
    
    Revert "Settings: improved stats upload logic"
    
    This reverts commit 7258fabb7c864f63285bf4d4874c185b65ada3d1.
    
    Revert "Settings: update community metrics"
    
    This reverts commit 0e05f466d0d4f1790542a3e33e9272d285c29c82.
    
    -----
    
    Change-Id: Ibf3b7c7c4303cb458ccdab6b2ef0548dfdd2ddbe
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- src/com/android/settings/cmstats/AnonymousStats.java
+++ src/com/android/settings/cmstats/AnonymousStats.java
@@ -20,27 +20,14 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import android.os.UserHandle;
-import android.preference.Preference;
-import android.preference.PreferenceScreen;
-import android.preference.SwitchPreference;
 import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import cyanogenmod.providers.CMSettings;
 
 public class AnonymousStats extends SettingsPreferenceFragment {
-
     private static final String PREF_FILE_NAME = "CMStats";
     /* package */ static final String ANONYMOUS_OPT_IN = "pref_anonymous_opt_in";
     /* package */ static final String ANONYMOUS_LAST_CHECKED = "pref_anonymous_checked_in";
-
-    /* package */ static final String KEY_LAST_JOB_ID = "last_job_id";
-    /* package */ static final int QUEUE_MAX_THRESHOLD = 1000;
-
-    public static final String KEY_STATS = "stats_collection";
-
-    SwitchPreference mStatsSwitch;
 
     public static SharedPreferences getPreferences(Context context) {
         return context.getSharedPreferences(PREF_FILE_NAME, 0);
@@ -50,52 +37,6 @@ public class AnonymousStats extends SettingsPreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.anonymous_stats);
-        mStatsSwitch = (SwitchPreference) findPreference(KEY_STATS);
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mStatsSwitch) {
-            boolean checked = mStatsSwitch.isChecked();
-            if (checked) {
-                // clear opt out flags
-                CMSettings.Secure.putIntForUser(getContentResolver(),
-                        CMSettings.Secure.STATS_COLLECTION_REPORTED, 0, UserHandle.USER_OWNER);
-            }
-            // will initiate opt out sequence if necessary
-            ReportingServiceManager.setAlarm(getActivity());
-            return true;
-        }
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
-    }
-
-    public static void updateLastSynced(Context context) {
-        getPreferences(context)
-                .edit()
-                .putLong(ANONYMOUS_LAST_CHECKED,System.currentTimeMillis())
-                .commit();
-    }
-
-    private static int getLastJobId(Context context) {
-        return getPreferences(context).getInt(KEY_LAST_JOB_ID, 0);
-    }
-
-    private static void setLastJobId(Context context, int id) {
-        getPreferences(context)
-                .edit()
-                .putInt(KEY_LAST_JOB_ID, id)
-                .commit();
-    }
-
-    public static int getNextJobId(Context context) {
-        int lastId = getLastJobId(context);
-        if (lastId >= QUEUE_MAX_THRESHOLD) {
-            lastId = 1;
-        } else {
-            lastId += 1;
-        }
-        setLastJobId(context, lastId);
-        return lastId;
     }
 
     @Override
